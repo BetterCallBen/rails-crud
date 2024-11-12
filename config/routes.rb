3f719bac31a9620d831ab6d06ac5fdb2286ad9db
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2024-11-12 11:50:00 +0100

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+  # resources :resources
 end
